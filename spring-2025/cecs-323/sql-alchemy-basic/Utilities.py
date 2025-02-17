377eeb17d6b22d87f38c09a91d76cdbc4b7d2724
Carlos Aguilera <18aguilerac@gmail.com>
2025-02-16 16:13:59 -0800

    update

---
+++ spring-2025/cecs-323/sql-alchemy-basic/Utilities.py
@@ -0,0 +1,32 @@
+from configparser import ConfigParser
+from sqlalchemy import create_engine, Engine
+from CarModel import CarModel
+config = ConfigParser()
+config.read('config.ini')
+
+
+def get_test_engine() -> Engine:
+    config = ConfigParser()
+    config.read('config.ini')  # the config.ini file has to be in the working directory.
+    userID: str = config['credentials']['userid']
+    password: str = config['credentials']['password']
+    host: str = config['credentials']['host']
+    port: str = config['credentials']['port']
+    database: str = config['credentials']['database']
+    # 'psycopg2' in this part of the db_url instructs SQLAlchemy that we are connecting to a PostgreSQL database.
+    db_url: str = f"postgresql+psycopg2://{userID}:{password}@{host}:{port}/{database}"
+    # db_url_display: str = f"postgresql+psycopg2://{userID}:********@{host}:{port}/{database}"
+    # The engine is the connection to the PostgreSQL database
+    return create_engine(db_url, pool_size=5, pool_recycle=3600, echo=False)
+
+
+def get_Ford() -> CarModel:
+    """
+    This just stuffs some representative values into an instance of CarModel so that I don't
+    have to do this repeatedly in my test cases.
+    :return: A canned instance of CarModel.
+    """
+    car = CarModel('Ford', 'Taurus', 1984, 25,
+                   30, 'manual', 1, 350, 0,
+                   20000.35)
+    return car
