c166caff5b5b226ac6b950cf41b691b50d174180
Carlos Aguilera <18aguilerac@gmail.com>
2025-02-16 23:41:50 -0800

    remove comments

--- spring-2025/cecs-323/sql-alchemy-basic/test_schema.py
+++ spring-2025/cecs-323/sql-alchemy-basic/test_schema.py
@@ -18,41 +18,32 @@ def get_schema() -> Schema:
 @pytest.fixture(scope='function')
 def db_session() -> Generator[Session]:
     config = ConfigParser()
-    config.read('config.ini')  # the config.ini file has to be in the working directory.
+    config.read('config.ini')
+
     log_level = eval(config['logging']['level'])
-    # log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
     logging.getLogger("sqlalchemy.engine").setLevel(log_level)
     logging.getLogger("sqlalchemy.pool").setLevel(log_level)
+
     engine = get_test_engine()
     metadata.drop_all(bind=engine)
     metadata.create_all(bind=engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    # "return" the session that we've just created.
+
     yield session
-    # Then control comes back here where we wipe out the schema.
+
     session.close()
 
 
 def test_insert_schema(db_session: Session):
-    """
-    See whether we can insert a car model into the database and verify that it is
-    there by searching for it.
-    :param db_session: The database connection.
-    :return: None
-    """
-    # So far, car is just a database instance in memory, nothing more.
     schema = get_schema()
-    # In SQLAlchemy, everything happens in the session.  In Beanie, it's the objects
-    # themselves that inherit the necessary methods.
+
     db_session.add(schema)
-    # perform the insert in the database.  But the new row is still not visible to other
-    # sessions until we commit.  The commit operation performs an implicit flush().
     db_session.flush()
-    # We know that we started with 0 rows in the table, so the one we just inserted must be the only one.
+
     count = db_session.query(Schema).filter_by(name=schema.name).count()
-    # check to make sure that there is one and only one row in the car_model table.
     assert count == 1
 
 
