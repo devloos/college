b7be7a809358eeb79419cfda2f273c389ba9d8b5
Carlos Aguilera <18aguilerac@gmail.com>
2025-02-23 16:44:20 -0800

    prog 2 done

--- spring-2025/cecs-325/prog-2/war.cpp
+++ spring-2025/cecs-325/prog-2/war.cpp
@@ -1,16 +1,18 @@
 // Name: Carlos Aguilera
 // Class CECS 325-02
-// Project Name Prog 1 – Card War
-// Due Date 02/11/2025
+// Project Name Prog 2 – More Card War
+// Due Date 02/23/2025
 //
 // I certify that this program is my own original work. I did not copy any part
 // of this program from any other source. I further certify that I typed each
 // and every line of code in this program.
 
 #include <algorithm>
+#include <array>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -18,40 +20,25 @@
 const int WON_GAME = 1;
 const int TIE_GAME = 0;
 
-const int NUMBER_OF_CARDS = 52;
-const int NUMBER_OF_SUITS = 4;
-const int NUMBER_OF_RANKS = 13;
-
-const char SUITS[] = {'C', 'S', 'D', 'H'};
-const char RANKS[] = {'A', '2', '3', '4', '5', '6', '7',
-                      '8', '9', 'X', 'J', 'Q', 'K'};
+const std::array<char, 4> SUITS = {'C', 'S', 'D', 'H'};
+const std::array<char, 13> RANKS = {'A', '2', '3', '4', '5', '6', '7',
+                                    '8', '9', 'X', 'J', 'Q', 'K'};
 
 class Card {
  public:
-  Card() {
-    this->suit = 'X';
-    this->rank = 'X';
-  }
-
   Card(char suit, char rank) {
     this->suit = suit;
     this->rank = rank;
   }
 
-  void displayCard() const {
-    if (this->rank == 'X') {
-      std::cout << "10" << this->suit;
-    } else {
-      std::cout << this->rank << this->suit;
-    }
-  }
+  void displayCard() const { std::cout << rank << suit; }
 
-  int compareCard(const Card &other) const {
+  int compareCard(const Card& other) const {
     int thisRankNum = 0;
     int otherRankNum = 0;
 
-    for (int i = 0; i < NUMBER_OF_RANKS; ++i) {
-      if (this->rank == RANKS[i]) {
+    for (int i = 0; i < RANKS.size(); ++i) {
+      if (rank == RANKS[i]) {
         thisRankNum = i;
       }
 
@@ -83,46 +70,46 @@ struct Player {
 class Deck {
  public:
   Deck() {
-    for (int suitIdx = 0; suitIdx < NUMBER_OF_SUITS; ++suitIdx) {
-      for (int rankIdx = 0; rankIdx < NUMBER_OF_RANKS; ++rankIdx) {
-        this->deck.push_back(Card(SUITS[suitIdx], RANKS[rankIdx]));
+    for (char suit : SUITS) {
+      for (char rank : RANKS) {
+        deck.push_back(Card(suit, rank));
       }
     }
   }
 
   Card deal() {
-    Card temp = this->deck.back();
-    this->deck.pop_back();
+    if (deck.size() == 0) {
+      throw "Error - Deck is empty";
+    }
+
+    Card temp = deck.back();
+    deck.pop_back();
     return temp;
   }
 
   void displayDeck() {
-    for (int suitIdx = 0; suitIdx < NUMBER_OF_SUITS; ++suitIdx) {
-      for (int rankIdx = 0; rankIdx < NUMBER_OF_RANKS; ++rankIdx) {
-        // number of ranks determine offset, suitIdx determine how big
-        // the offset will be
-        const int index = suitIdx * NUMBER_OF_RANKS + rankIdx;
-        this->deck[index].displayCard();
+    for (int i = 0; i < deck.size(); ++i) {
+      deck[i].displayCard();
 
-        if (rankIdx < 13) {
-          std::cout << ',';
-        }
+      if ((i + 1) % 13 == 0) {
+        std::cout << '\n';
+      } else {
+        std::cout << ',';
       }
-      std::cout << '\n';
     }
   }
 
   void shuffle() {
     std::srand(std::time(0));
 
-    for (int i = 0; i < NUMBER_OF_CARDS; ++i) {
+    for (int i = 0; i < deck.size(); ++i) {
       // grab new index using i as an increment to skip previous cards
-      int newIndex = rand() % (NUMBER_OF_CARDS - i + 1);
+      int newIndex = rand() % (deck.size() - i + 1);
 
       // perform swap
-      Card temp = this->deck[i];
-      this->deck[i] = this->deck[newIndex];
-      this->deck[newIndex] = temp;
+      Card temp = deck[i];
+      deck[i] = deck[newIndex];
+      deck[newIndex] = temp;
     }
   }
 
@@ -140,6 +127,11 @@ int main() {
   std::cout << "Enter the name of the second player: ";
   std::getline(std::cin, secondPlayer.name);
 
+  int numberOfGames = 0;
+
+  std::cout << "How many games will they play? ";
+  std::cin >> numberOfGames;
+
   Deck deck;
 
   std::cout << '\n';
@@ -156,42 +148,46 @@ int main() {
 
   std::cout << '\n';
 
-  for (int i = 0; i < NUMBER_OF_CARDS / 2; ++i) {
-    firstPlayer.currentCard = deck.deal();
-    secondPlayer.currentCard = deck.deal();
+  try {
+    for (int i = 0; i < numberOfGames; ++i) {
+      firstPlayer.currentCard = deck.deal();
+      secondPlayer.currentCard = deck.deal();
 
-    std::cout << "Game " << i + 1 << '\n';
-    std::cout << "--------" << '\n';
+      std::cout << "Game " << i + 1 << '\n';
+      std::cout << "--------" << '\n';
 
-    std::cout << firstPlayer.name << "=>";
-    firstPlayer.currentCard.displayCard();
-    std::cout << '\n';
+      std::cout << firstPlayer.name << "=>";
+      firstPlayer.currentCard.displayCard();
+      std::cout << '\n';
 
-    std::cout << secondPlayer.name << "=>";
-    secondPlayer.currentCard.displayCard();
-    std::cout << '\n';
+      std::cout << secondPlayer.name << "=>";
+      secondPlayer.currentCard.displayCard();
+      std::cout << '\n';
 
-    const int firstPlayerResult =
-        firstPlayer.currentCard.compareCard(secondPlayer.currentCard);
+      const int firstPlayerResult =
+          firstPlayer.currentCard.compareCard(secondPlayer.currentCard);
 
-    if (firstPlayerResult == WON_GAME) {
-      std::cout << firstPlayer.name << "=> Winner";
+      if (firstPlayerResult == WON_GAME) {
+        std::cout << firstPlayer.name << "=> Winner";
 
-      ++firstPlayer.wins;
-      ++secondPlayer.losses;
-    } else if (firstPlayerResult == TIE_GAME) {
-      std::cout << "Tie game";
+        ++firstPlayer.wins;
+        ++secondPlayer.losses;
+      } else if (firstPlayerResult == TIE_GAME) {
+        std::cout << "Tie game";
 
-      ++firstPlayer.ties;
-      ++secondPlayer.ties;
-    } else {
-      std::cout << secondPlayer.name << "=> Winner";
+        ++firstPlayer.ties;
+        ++secondPlayer.ties;
+      } else {
+        std::cout << secondPlayer.name << "=> Winner";
 
-      ++firstPlayer.losses;
-      ++secondPlayer.wins;
+        ++firstPlayer.losses;
+        ++secondPlayer.wins;
+      }
+
+      std::cout << "\n\n";
     }
-
-    std::cout << "\n\n";
+  } catch (const char* message) {
+    std::cout << message << "\n\n";
   }
 
   // is there a better way to manipulate text this is absolutely HORRIBLE
