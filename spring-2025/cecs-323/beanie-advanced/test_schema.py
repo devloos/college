c5212e91336298444af6c88987d78362135da5a6
Carlos Aguilera <18aguilerac@gmail.com>
2025-03-06 20:36:40 -0800

    fix async

--- spring-2025/cecs-323/beanie-advanced/test_schema.py
+++ spring-2025/cecs-323/beanie-advanced/test_schema.py
@@ -1,8 +1,9 @@
+from typing import AsyncGenerator
 import pytest
 from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorClientSession
 from pydantic import ValidationError
-from pymongo.errors import DuplicateKeyError, OperationFailure
+from pymongo.errors import DuplicateKeyError
 
 from schema import Schema
 from datetime import datetime
@@ -21,103 +22,94 @@ def get_schema():
     return schema_dict
 
 
-@pytest_asyncio.fixture
-async def client():
+@pytest_asyncio.fixture()
+async def session() -> AsyncGenerator[AsyncIOMotorClientSession]:
     motor_client = AsyncIOMotorClient(
         "mongodb://localhost:27018/?directConnection=true&replicaSet=rs0&w=majority"
     )
     # Be sure to change the database name to whatever you want in your MongoDB database.
     db = motor_client.get_database("BeanieDemo")
     await init_beanie(database=db, document_models=[Schema])
-    return motor_client
+
+    async with await motor_client.start_session() as session:
+        async with session.start_transaction():
+            yield session
 
 
 @pytest.mark.asyncio
-async def test_insert_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_dict = get_schema()
-            schema_model = Schema(**schema_dict)
+async def test_insert_schema(session: AsyncIOMotorClientSession):
+    schema_dict = get_schema()
+    schema_model = Schema(**schema_dict)
 
-            await schema_model.insert(session=session)
-            schemas = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
+    await schema_model.insert(session=session)
+    schemas = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
 
-            assert len(schemas) == 1
+    assert len(schemas) == 1
 
-            await schema_model.delete(session=session)
+    await schema_model.delete(session=session)
 
 
 @pytest.mark.asyncio
-async def test_bad_type_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_dict = get_schema()
-            schema_dict['createdAt'] = "not a time"
+async def test_bad_type_schema(session: AsyncIOMotorClientSession):
+    schema_dict = get_schema()
+    schema_dict['createdAt'] = "not a time"
 
-            with pytest.raises(ValidationError) as SE:
-                schema_model = Schema.model_validate(schema_dict)
-                await schema_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_model = Schema.model_validate(schema_dict)
+        await schema_model.insert(session=session)
 
 
 @pytest.mark.asyncio
-async def test_string_too_long_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_dict = get_schema()
-            schema_dict['name'] = '0' * 84
+async def test_string_too_long_schema(session: AsyncIOMotorClientSession):
+    schema_dict = get_schema()
+    schema_dict['name'] = '0' * 84
 
-            with pytest.raises(ValidationError) as SE:
-                schema_model = Schema(**schema_dict)
-                await schema_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_model = Schema(**schema_dict)
+        await schema_model.insert(session=session)
 
 
 @pytest.mark.asyncio
-async def test_delete_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_dict = get_schema()
-            schema_model = Schema(**schema_dict)
+async def test_delete_schema(session: AsyncIOMotorClientSession):
+    schema_dict = get_schema()
+    schema_model = Schema(**schema_dict)
 
-            await schema_model.insert(session=session)
+    await schema_model.insert(session=session)
 
-            schema_models = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
+    schema_models = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
 
-            assert len(schema_models) == 1
+    assert len(schema_models) == 1
 
-            schema_model = await Schema.find_one(Schema.name == schema_model.name, session=session)
+    schema_model = await Schema.find_one(Schema.name == schema_model.name, session=session)
 
-            await schema_model.delete(session=session)
+    await schema_model.delete(session=session)
 
-            schema_models = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
+    schema_models = await Schema.find(Schema.name == schema_model.name, session=session).to_list()
 
-            assert len(schema_models) == 0
+    assert len(schema_models) == 0
 
 
 @pytest.mark.asyncio
-async def test_duplicate_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            # insert first schema
-            schema_dict = get_schema()
-            schema_model = Schema(**schema_dict)
+async def test_duplicate_schema(session: AsyncIOMotorClientSession):
+    # insert first schema
+    schema_dict = get_schema()
+    schema_model = Schema(**schema_dict)
 
-            await schema_model.insert(session=session)
+    await schema_model.insert(session=session)
 
-            # insert second schema
-            try:
-                schema_model = Schema(**schema_dict.copy())
-                await schema_model.insert(session=session)
-            except DuplicateKeyError as DKE:
-                await session.abort_transaction()
+    # insert second schema
+    try:
+        schema_model = Schema(**schema_dict.copy())
+        await schema_model.insert(session=session)
+    except DuplicateKeyError as DKE:
+        await session.abort_transaction()
 
 
 @pytest.mark.asyncio
-async def test_name_too_short_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_dict = get_schema()
-            schema_dict['name'] = 'SHRT'
+async def test_name_too_short_schema(session: AsyncIOMotorClientSession):
+    schema_dict = get_schema()
+    schema_dict['name'] = 'SHRT'
 
-            with pytest.raises(ValidationError) as SE:
-                schema_model = Schema(**schema_dict)
-                await schema_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_model = Schema(**schema_dict)
+        await schema_model.insert(session=session)
--- spring-2025/cecs-323/beanie-advanced/test_schema_object.py
+++ spring-2025/cecs-323/beanie-advanced/test_schema_object.py
@@ -1,11 +1,24 @@
+from typing import AsyncGenerator
 import pytest
-from beanie import init_beanie, BeanieObjectId
-from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorClientSession
 from pydantic import ValidationError
+from pymongo.errors import DuplicateKeyError
 
 from datetime import datetime
 from schema_object import SchemaObject
+from schema import Schema
 import pytest_asyncio
+
+
+def get_schema():
+    schema_dict = {
+        'name': 'My Schema',
+        'description': 'A description of my schema',
+        'createdAt': datetime(2000, 12, 1),
+    }
+
+    return schema_dict
 
 
 def get_schema_object():
@@ -20,77 +33,78 @@ def get_schema_object():
 
 
 @pytest_asyncio.fixture()
-async def client():
+async def session() -> AsyncGenerator[AsyncIOMotorClientSession]:
     motor_client = AsyncIOMotorClient(
         "mongodb://localhost:27018/?directConnection=true&replicaSet=rs0&w=majority"
     )
     # Be sure to change the database name to whatever you want in your MongoDB database.
     db = motor_client.get_database("BeanieDemo")
-    await init_beanie(database=db, document_models=[SchemaObject])
+    await init_beanie(database=db, document_models=[Schema, SchemaObject])
 
-    return motor_client
+    async with await motor_client.start_session() as session:
+        async with session.start_transaction():
+            schema_dict = get_schema()
+            schema_model = Schema(**schema_dict)
+
+            await schema_model.insert(session=session)
+
+            yield session
+
+            await schema_model.delete(session=session)
 
 
 @pytest.mark.asyncio
-async def test_insert_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_object_dict = get_schema_object()
-            schema_object_model = SchemaObject(**schema_object_dict)
+async def test_insert_schema(session: AsyncIOMotorClientSession):
+    schema_object_dict = get_schema_object()
+    schema_object_model = SchemaObject(**schema_object_dict)
 
-            await schema_object_model.insert(session=session)
+    await schema_object_model.insert(session=session)
 
-            schema_objects = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
+    schema_objects = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
 
-            assert len(schema_objects) == 1
+    assert len(schema_objects) == 1
 
-            await schema_object_model.delete(session=session)
+    await schema_object_model.delete(session=session)
 
 
 @pytest.mark.asyncio
-async def test_bad_type_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_object_dict = get_schema_object()
-            schema_object_dict['createdAt'] = "not a time"
+async def test_bad_type_schema(session: AsyncIOMotorClientSession):
+    schema_object_dict = get_schema_object()
+    schema_object_dict['createdAt'] = "not a time"
 
-            with pytest.raises(ValidationError) as SE:
-                schema_object_model = SchemaObject.model_validate(schema_object_dict)
-                await schema_object_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_object_model = SchemaObject.model_validate(schema_object_dict)
+        await schema_object_model.insert(session=session)
 
 
 @pytest.mark.asyncio
-async def test_string_too_long_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_object_dict = get_schema_object()
-            schema_object_dict['name'] = '0' * 84
+async def test_string_too_long_schema(session: AsyncIOMotorClientSession):
+    schema_object_dict = get_schema_object()
+    schema_object_dict['name'] = '0' * 84
 
-            with pytest.raises(ValidationError) as SE:
-                schema_object_model = SchemaObject(**schema_object_dict)
-                await schema_object_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_object_model = SchemaObject(**schema_object_dict)
+        await schema_object_model.insert(session=session)
 
 
 @pytest.mark.asyncio
-async def test_delete_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_object_dict = get_schema_object()
-            schema_object_model = SchemaObject(**schema_object_dict)
+async def test_delete_schema(session: AsyncIOMotorClientSession):
+    schema_object_dict = get_schema_object()
+    schema_object_model = SchemaObject(**schema_object_dict)
 
-            await schema_object_model.insert(session=session)
+    await schema_object_model.insert(session=session)
 
-            schema_object_models = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
+    schema_object_models = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
 
-            assert len(schema_object_models) == 1
+    assert len(schema_object_models) == 1
 
-            schema_object_model = await SchemaObject.find_one(SchemaObject.name == schema_object_model.name, session=session)
+    schema_object_model = await SchemaObject.find_one(SchemaObject.name == schema_object_model.name, session=session)
 
-            await schema_object_model.delete(session=session)
+    await schema_object_model.delete(session=session)
 
-            schema_object_models = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
+    schema_object_models = await SchemaObject.find(SchemaObject.name == schema_object_model.name, session=session).to_list()
 
-            assert len(schema_object_models) == 0
+    assert len(schema_object_models) == 0
 
 
 # def test_non_existent_schema(db_session: Session):
@@ -138,6 +152,21 @@ async def test_delete_schema(client: AsyncIOMotorClient):
 #     assert str(IE).find(
 #         'duplicate key value violates unique constraint "schema_objects_pk_01"') > -1
 
+@pytest.mark.asyncio
+async def test_duplicate_schema(session: AsyncIOMotorClientSession):
+    # insert first schema object
+    schema_object_dict = get_schema_object()
+    schema_object_model = SchemaObject(**schema_object_dict)
+
+    await schema_object_model.insert(session=session)
+
+    # insert second schema object
+    try:
+        schema_object_model = SchemaObject(**schema_object_dict.copy())
+        await schema_object_model.insert(session=session)
+    except DuplicateKeyError as DKE:
+        await session.abort_transaction()
+
 
 # def test_duplicate_schema_object_different_parents(db_session: Session):
 #     # Create schema object
@@ -161,12 +190,10 @@ async def test_delete_schema(client: AsyncIOMotorClient):
 
 
 @pytest.mark.asyncio
-async def test_name_too_short_schema(client: AsyncIOMotorClient):
-    async with await client.start_session() as session:
-        async with session.start_transaction():
-            schema_object_dict = get_schema_object()
-            schema_object_dict['name'] = 'SHRT'
+async def test_name_too_short_schema(session: AsyncIOMotorClientSession):
+    schema_object_dict = get_schema_object()
+    schema_object_dict['name'] = 'SHRT'
 
-            with pytest.raises(ValidationError) as SE:
-                schema_model = SchemaObject(**schema_object_dict)
-                await schema_model.insert(session=session)
+    with pytest.raises(ValidationError) as SE:
+        schema_model = SchemaObject(**schema_object_dict)
+        await schema_model.insert(session=session)
