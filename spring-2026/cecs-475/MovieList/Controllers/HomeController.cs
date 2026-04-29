ca9b95418d178286a18735a627153ef8444f4cf5
Carlos Aguilera <18aguilerac@gmail.com>
2026-04-29 01:15:30 -0700

    Enhance HomeController and Index view to display a list of movies. Implement dependency injection for MovieContext and update the Index view to show movie details in a structured table format with options to edit and delete.

--- spring-2026/cecs-475/MovieList/Controllers/HomeController.cs
+++ spring-2026/cecs-475/MovieList/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using MovieList.Models;
 
 namespace MovieList.Controllers;
 
 public class HomeController : Controller
 {
+    private MovieContext context { get; set; }
+    public HomeController(MovieContext ctx) => context = ctx;
     public IActionResult Index()
     {
-        return View();
+        var movies = context.Movies
+        .OrderBy(m => m.Name) //Order by name
+        .ToList();
+
+        return View(movies);
     }
 }
