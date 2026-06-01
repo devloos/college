37bedd310c514ec26d9b4e745ff3f58ba8877332
Carlos Aguilera <18aguilerac@gmail.com>
2026-05-31 17:11:59 -0700

    update

--- summer-2026/cecs-453/flutter-course/coffee_card/lib/main.dart
+++ summer-2026/cecs-453/flutter-course/coffee_card/lib/main.dart
@@ -2,5 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:coffee_card/home.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Home()));
+  runApp(const MaterialApp(home: Sandbox()));
+}
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(width: 100, height: 100, color: Colors.red),
+          Container(width: 100, height: 100, color: Colors.blue),
+          Row(
+            children: [
+              Container(width: 100, height: 100, color: Colors.green),
+              Container(width: 100, height: 100, color: Colors.yellow),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
