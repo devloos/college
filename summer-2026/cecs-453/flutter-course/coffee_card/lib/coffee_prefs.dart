0befe2e9b37a076d166a4b59556e5f44a8225031
Carlos Aguilera <18aguilerac@gmail.com>
2026-05-31 19:11:20 -0700

    Refactor CoffeePrefs to use StatefulWidget, enhancing interactivity with dynamic strength and sugar displays. Removed unnecessary print statements and optimized UI rendering.

--- summer-2026/cecs-453/flutter-course/coffee_card/lib/coffee_prefs.dart
+++ summer-2026/cecs-453/flutter-course/coffee_card/lib/coffee_prefs.dart
@@ -1,30 +1,66 @@
 import 'package:flutter/material.dart';
 
-class CoffeePrefs extends StatelessWidget {
+class CoffeePrefs extends StatefulWidget {
   const CoffeePrefs({super.key});
 
+  @override
+  State<CoffeePrefs> createState() => _CoffeePrefsState();
+}
+
+class _CoffeePrefsState extends State<CoffeePrefs> {
+  int strength = 1;
+  int sugars = 1;
+
   void incrementStrength() {
-    print('incrementStrength');
+    setState(() {
+      strength = (strength % 5) + 1;
+    });
   }
 
   void incrementSugars() {
-    print('incrementSugars');
+    setState(() {
+      sugars = ((sugars + 1) % 6);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final coffeeBeanImage = Image.asset(
+      'assets/img/coffee_bean.png',
+      width: 25,
+      color: Colors.brown[100],
+      colorBlendMode: BlendMode.multiply,
+    );
+
+    List<Widget> strengthImages = [];
+
+    for (int i = 0; i < strength; i++) {
+      strengthImages.add(coffeeBeanImage);
+    }
+
+    final sugarImage = Image.asset(
+      'assets/img/sugar_cube.png',
+      width: 25,
+      color: Colors.brown[100],
+      colorBlendMode: BlendMode.multiply,
+    );
+
+    List<Widget> sugarImages = [];
+
+    if (sugars == 0) {
+      sugarImages.add(Text('No sugar'));
+    }
+
+    for (int i = 0; i < sugars; i++) {
+      sugarImages.add(sugarImage);
+    }
+
     return Column(
       children: [
         Row(
           children: [
             Text('Strength: '),
-            Text('1'),
-            Image.asset(
-              'assets/img/coffee_bean.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            ...strengthImages,
             Expanded(child: SizedBox()),
             ElevatedButton(
               onPressed: incrementStrength,
@@ -39,13 +75,7 @@ class CoffeePrefs extends StatelessWidget {
         Row(
           children: [
             Text('Sugars: '),
-            Text('2'),
-            Image.asset(
-              'assets/img/sugar_cube.png',
-              width: 25,
-              color: Colors.brown[100],
-              colorBlendMode: BlendMode.multiply,
-            ),
+            ...sugarImages,
             Expanded(child: SizedBox()),
             ElevatedButton(
               onPressed: incrementSugars,
